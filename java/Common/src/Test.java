ff7c44c0178168b72fd139f5e4a655a9ce2e0eda
Y65NG <2676409153@qq.com>
2021-02-02 00:24:47 +0800

    add Test.java

--- java/Common/src/Test.java
+++ java/Common/src/Test.java
@@ -1,7 +1,11 @@
-public class Test {
-    public static void main(String[] args) {
-        int[] arr = {1};
-        int[][] arr2 = {arr};
-        
-    }
+import java.util.*;
+
+public class Test{
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		int[] arr = new int[100000000];
+		for(int i = 0; i < arr.length; i ++) arr[i] = 1;
+		long end = System.currentTimeMillis();
+		System.out.println(end);
+	}
 }
